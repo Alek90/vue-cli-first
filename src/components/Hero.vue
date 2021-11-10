3d16b7a3eb23e3e715a88d72b06f28d9e45f37b4
Alek90 <4lek.90@gmail.com>
2021-11-10 17:00:52 +0100

    Creato nuovo componente Hero.vue.

---
+++ src/components/Hero.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="hero">
+
+        <img alt="Vue logo" src="../assets/logo.png">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Hero'
+}
+</script>
+
+<style scoped>
+    
+</style>
