ade531da33e79816a6a08d8f8cacf6d1e1cbbce3
Alek90 <4lek.90@gmail.com>
2021-11-10 18:44:05 +0100

    Consegna rivista.
    Rivista componente Hero in funzione di coerenza con un jumbotron.

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,7 +3,17 @@
 
         <h2>JUMBOTRON PER VUE CLI</h2>
 
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores odit vitae officiis totam, quidem consequatur quos hic amet nihil optio reiciendis officia itaque accusantium sequi iure nostrum commodi excepturi in quasi atque mollitia labore quis. Libero deleniti omnis placeat! In earum minus, nisi, cumque quis consectetur sapiente excepturi dicta molestias incidunt itaque autem suscipit eaque modi at! Reprehenderit nisi magnam optio voluptate natus odit ut repellendus et veniam, exercitationem facere inventore ipsa dolores maxime neque.</p>
+        <div class="option">
+
+            <p class="left">
+                Lorem ipsum dolor sit amet.
+            </p>
+
+            <p class="right">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta reprehenderit nobis vitae cumque repudiandae nisi est et.
+            </p>
+
+        </div>
 
     </div>
 </template>
@@ -16,18 +26,38 @@ export default {
 
 <style scoped>
 .hero{
-    height: 750px;
+    width: 570px;
+    height: 300px;
     margin:0 auto;
-    padding: 100px 175px;
+    padding: 35px;
     text-align: center;
     color: whitesmoke;
     background-image: url(https://images.ctfassets.net/nj2caiz7hkjw/5SdA2VBLpKUsAeumA2EsMa/537af179a90aba7a0ce3f823d9d69408/form-with-vuejs.jpg);
     background-size: cover;
     background-repeat: no-repeat;
-    background-position: top;
+    background-position: center;
+    border-radius: 25px;
 }
 
 h2{
     line-height: 135px;
+    text-decoration: underline;
+}
+
+.option{
+    display: flex;
+    justify-content: space-evenly;
+}
+
+p{
+    line-height: 1.5rem;
+}
+
+.left{
+    width: 30%;
+}
+
+.right{
+    width: 55%;
 }
 </style>
